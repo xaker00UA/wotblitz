74df70cc0229bcc6ab5433179c74bd12ba4a2c2d
Ivan <ivanbozhkod@gmail.com>
2024-07-15 15:54:16 +0300

    fix datebase

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ with open("config.json", "r") as f:
 
 LIMITED = config.get("limited", 10)
 APPLICATION_ID = config.get("application_id", "6645d2ba41b7ded38e934bd6fdd48d05")
-DATABASE = config.get("database", "mongodb://localhost:27017/")
+DATABASE = config.get(
+    "database", "mongodb+srv://test:1234@wotblitz.9ctju87.mongodb.net/"
+)
 SESSION = config.get("start_session", False)
 CLANS = config.get("clans", [])
 PLAYERS = config.get("players", [])
